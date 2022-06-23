575f48c91a9d449f8fb40ae24c366b6dd492c893
xaur <24484727+xaur@users.noreply.github.com>
2022-06-23 21:57:15 +0000

    iu: code nits
    - Double blanks around fns.
    - Sentence case output and comments.
    - Simplify argparser fallback.
    - Print more activity.

--- iu.py
+++ iu.py
@@ -1,36 +1,43 @@
-import os
-
 from datetime import datetime
+import os
 from urllib.parse import urlparse
 
 import strictyaml
 
+
+class Config:
+    indexdir    = "index"
+    builddir    = "index"
+
+
 class IuError(Exception):
     pass
 
-class Config:
-    indexdir   = "index"
-    builddir   = "index"
 
 class Const:
     index       = "index"
+
 
 def load_str(path):
     with open(path) as f:
         return f.read()
 
+
 def load_yaml(path):
     return strictyaml.load(load_str(path))
 
+
 def write_str(path, s):
-    # overwrite existing file
+    # Overwrite existing file.
     with open(path, "w", newline="\n") as f:
         f.write(s)
+
 
 def init_build_dir(path):
     if not os.path.exists(path):
         os.mkdir(path, mode=0o700)
-        print("created directory:", path)
+        print("Created directory:", path)
+
 
 def parse_date(s):
     for fmt in ["%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M", "%Y-%m-%d"]:
@@ -38,7 +45,8 @@ def parse_date(s):
             return datetime.strptime(s, fmt)
         except ValueError:
             continue
-    raise IuError("unknown date format: " + s)
+    raise IuError("Unknown date format: " + s)
+
 
 LANGUAGES = {
     "ar": "Arabic",
@@ -49,6 +57,7 @@ LANGUAGES = {
     "zh": "Chinese",
 }
 
+
 SITES = {
     "facebook.com"  : "Facebook",
     "instagram.com" : "Instagram",
@@ -58,21 +67,26 @@ SITES = {
     "youtube.com"   : "YouTube",
 }
 
+
 def hostname(url):
     host = urlparse(url).hostname
-    # strip www.
+    # Strip www.
     hostnw = host[len("www."):] if host.startswith("www.") else host
     return hostnw
 
+
 def site_name(url):
     host = hostname(url)
+    name = host
     if host in SITES:
-        return SITES[host]
-    return host
+        name = SITES[host]
+    return name
+
 
 def twitter_username(url):
     segments = urlparse(url).path.split("/")
     return segments[1]
+
 
 def list_md(items, indent=0):
     md = ""
@@ -81,11 +95,14 @@ def list_md(items, indent=0):
         md += indentstr + "- {}\n".format(it)
     return md
 
+
 def paragraph_md(p):
     md = p
     if not p.endswith("\n"):
-        md += "\n"      # if it doesn't have its own line ending, add it
+        # If `p` doesn't have its own line ending, add it.
+        md += "\n"
     return md
+
 
 def announcements_md(anns, indent=0):
     md = ""
@@ -102,6 +119,7 @@ def announcements_md(anns, indent=0):
         md += indentstr + "- [{}]({})\n".format(site_name(url), url)
     return md
 
+
 def media_md(medias, indent=0):
     md = ""
     indentstr = "  " * indent
@@ -113,6 +131,7 @@ def media_md(medias, indent=0):
         md += indentstr + "- [{}]({})\n".format(site_name(murl), murl)
     return md
 
+
 def titlesx_md(data, lang_codes):
     md = ""
     for code in lang_codes:
@@ -122,22 +141,23 @@ def titlesx_md(data, lang_codes):
                 md += "- title in {}: {}\n".format(LANGUAGES[code], titlex)
     return md
 
+
 def entry_md(eid, data):
-    """Render one index entry as Markdown"""
+    """Render one index entry as Markdown."""
     md = "back to [index]({}.md)\n\n".format(Const.index)
-    # required fields are accessed without testing to trigger an error if
-    # missing
+    # Required fields are accessed without testing to trigger an error if
+    # missing.
     md += "# {}\n\n".format(data["title"])
-    # begin key stats
+    # Begin key stats.
     langs_str = data["lang"]
     lang_codes = langs_str.split(", ")
     if not lang_codes:
-        raise IuError("at least one language is required in `lang`")
+        raise IuError("At least 1 language is required in `lang`")
     lang_names = []
     for code in lang_codes:
         lang_name = LANGUAGES.get(code)
         if not lang_name:
-            raise IuError("unknown language code: " + code)
+            raise IuError("Unknown language code: " + code)
         lang_names.append(lang_name)
 
     md += "- language: {}\n".format(", ".join(lang_names))
@@ -157,7 +177,7 @@ def entry_md(eid, data):
         orgs.append(ostr)
     md += "- organizers: {}\n".format(", ".join(orgs))
     md += "- Decred participants: {}\n".format(", ".join(data["decred_people"]))
-    # end key stats
+    # End key stats.
     if "description" in data:
         md += "\n"
         desc = data["description"]
@@ -185,7 +205,7 @@ def entry_md(eid, data):
             subtitle = subevent["title"]
             md += "\n"
             md += "### {}\n\n".format(subtitle)
-            # begin key stats
+            # Begin key stats.
             subtitlesx = titlesx_md(subevent, lang_codes)
             if subtitlesx:
                 md += subtitlesx
@@ -198,7 +218,7 @@ def entry_md(eid, data):
             subpresenters = subevent.get("presenters")
             if subpresenters:
                 md += "- presenters: {}\n".format(subpresenters)
-            # end key stats
+            # End key stats.
             subdesc = subevent.get("description")
             if subdesc:
                 md += "\n"
@@ -225,17 +245,19 @@ def entry_md(eid, data):
                 md += list_md(subnotes)
     return md
 
+
 def index_md(entries):
-    """Build top-level Markdown index page"""
+    """Build top-level Markdown index page."""
     md = ("# Decred Events\n\n"
-          "This is the index of past Decred events. "
-          "Pages are generated from YAML files. "
-          "To list your event please follow [these instructions](https://github.com/decredcommunity/events/blob/master/docs/submit-index.md).\n")
+          "This is the index of past Decred events."
+          " Pages are generated from YAML files."
+          " To list your event please follow [these instructions]("
+          "https://github.com/decredcommunity/events/blob/master/docs/submit-index.md).\n")
     year, month = None, None
     for eid, yaml in sorted(entries.items(), reverse=True):
         data = yaml.data
         date = parse_date(data["start_utc"])
-        # group by month
+        # Group by month.
         if not (year == date.year and month == date.month):
             year, month = date.year, date.month
             md += "\n## {}\n\n".format(date.strftime("%B %Y"))
@@ -247,11 +269,13 @@ def index_md(entries):
         md += item
     return md
 
+
 def write_md(outdir, basename, s):
     filename = basename + ".md"
     filepath = os.path.join(outdir, filename)
     write_str(filepath, s)
-    print("wrote", filename)
+    print("Wrote", filename)
+
 
 def build_md(args):
     outdir = args.outdir
@@ -268,58 +292,65 @@ def build_md(args):
                 yaml = load_yaml(filepath)
                 entries[eid] = yaml
 
-    print("writing to directory:", outdir)
+    print("Loaded {} entries".format(len(entries)))
+    print("Writing to directory:", outdir)
 
+    wrote_count = 0
     for eid, yaml in entries.items():
         try:
             emd = entry_md(eid, yaml.data)
             write_md(outdir, eid, emd)
+            wrote_count += 1
         except IuError as e:
-            print("error in {}: {}".format(eid, str(e)))
+            print("Error in {}: {}".format(eid, str(e)))
 
     write_md(outdir, Const.index, index_md(entries))
+    wrote_count += 1
+    print("Wrote {} files".format(wrote_count))
+
 
 def make_arg_parser():
     import argparse
 
-    parser = argparse.ArgumentParser(description="index utility")
+    parser = argparse.ArgumentParser(description="Index utility")
+    parser.set_defaults(cmd=lambda _args: parser.print_usage())
+
     subparsers = parser.add_subparsers(dest="command", title="commands")
 
     md = subparsers.add_parser(
         "md",
-        help="generate Markdown from index files")
+        help="Generate Markdown from index files")
     md.add_argument(
         "path", nargs="?",
         default=Config.indexdir,
-        help="index directory with input YAML files, '{}' by default"
-            .format(Config.indexdir))
+        help="Directory with input .yml files (default: {})"
+             "".format(Config.indexdir))
     md.add_argument(
         "-o", "--outdir",
         default=Config.builddir,
-        help="output dir for generated Markdown files, '{}' by default"
-            .format(Config.builddir))
-    md.set_defaults(func=build_md)
+        help="Output directory for generated .md files (default: {})"
+             "".format(Config.builddir))
+    md.set_defaults(cmd=build_md)
 
     return parser
+
 
 def main():
     parser = make_arg_parser()
     args = parser.parse_args()
 
-    if args.command:
-        try:
-            args.func(args)
-        except KeyboardInterrupt:
-            # handle Ctrl-C
-            print("\naborting")
-        except BrokenPipeError:
-            # silence error when e.g. piping into `less` and quitting before
-            # reading all
-            pass
-        except IuError as e:
-            print("error:", e)
-    else:
-        parser.print_usage()
+    try:
+        args.cmd(args)
+    except KeyboardInterrupt:
+        # Handle Ctrl-C.
+        print("\nAborting")
+    except BrokenPipeError:
+        # Silence error when e.g. piping into `less` and quitting before
+        # reading all.
+        pass
+    except IuError as e:
+        print("Error:", e)
+
 
 if __name__ == "__main__":
     main()
